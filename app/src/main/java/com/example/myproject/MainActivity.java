23ca3ef6a341346b4e09ae3c7219c03bef135c1a
Oscar <a18oscba@student.his.se>
2021-08-26 18:14:54 +0200

    TravelNations
    Created the class for travelNations and and arraylist and arrayadapter and the list the travelnations will be on.

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<TravelNations> travelList;
+    ArrayAdapter<TravelNations> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        travelList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, R.layout.list_item_textview, travelList);
+
+        ListView listView = findViewById(R.id.travelList);
+        listView.setAdapter(adapter);
     }
 }
